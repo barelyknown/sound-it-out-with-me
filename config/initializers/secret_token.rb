c33d5c38b375a68844b38e87dbbfa89ea83f5d8c
barelyknown <sean@buytruckload.com>
2013-12-09 22:09:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoundItOutWithMe::Application.config.secret_key_base = '23c1ef96aa861dbaaa95c955371082c35de15bb2d23f7d13ea52fe026329a21cb5b3d63f5a7980f296a5153bbb314d496ab46bd13803818878fb8ab959e5efdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoundItOutWithMe::Application.config.session_store :cookie_store, key: '_sound-it-out-with-me_session'
