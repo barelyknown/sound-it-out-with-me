b297cc7faddcdc95d79ca88192b66381255a8cd5
barelyknown <sean@buytruckload.com>
2013-12-10 00:06:37 -0600

    add basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SoundItOutWithMe::Application.routes.draw do
+  resources :phrases
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'phrases#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
